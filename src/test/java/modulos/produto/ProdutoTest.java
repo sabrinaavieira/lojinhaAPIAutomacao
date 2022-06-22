5dd8f28d8f27fc042d4c49c89ca9fa32e1a90b82
Sabrina Vieira <sabrinaaraujojean@gmail.com>
2022-06-21 23:20:39 -0300

    Corrigindo o path para a lojinha ao inves da lojinha bugada

--- src/test/java/modulos/produto/ProdutoTest.java
+++ src/test/java/modulos/produto/ProdutoTest.java
@@ -19,7 +19,7 @@ public class ProdutoTest {
     public void beforeEach() {
         // Configurando os dados da API Rest da Lojinha
         baseURI = "http://165.227.93.41";
-        basePath = "/lojinha-bugada";
+        basePath = "/lojinha";
 
 
 
